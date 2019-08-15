63ccfd2ae853c11912fd445ae045d00bbe51f76b
Dan Wasyluk <Dan Wasyluk 987186+dwasyluk@users.noreply.github.com>
2019-08-15 15:43:49 -0400

    getinputs wip

--- app.js
+++ app.js
@@ -135,11 +135,11 @@ app.use('/ext/connections', function(req,res){
 
 // this takes an array of { [txid]: voutindex } and returns the input objects reflected by that, mapped to the txid { [txid]: input }
 app.use('/ext/getinputs', (req,res) => {
-   console.log("getinputs:", req);
+   console.log("getinputs:", req.body);
 
-  for (let [key, value] of Object.entries(inputs)) {
-    console.log(`${key}: ${value}`);
-  }
+  //for (let [key, value] of Object.entries(inputs)) {
+  //  console.log(`${key}: ${value}`);
+  //}
 
   res.send({});
 });
