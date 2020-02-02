afbcf7c1a360a37c926e283fc8f84e199106ff74
Dan Wasyluk <987186+dwasyluk@users.noreply.github.com>
2020-02-02 18:10:26 -0500

    attempt smart sync on new block

--- smartsync.js
+++ smartsync.js
@@ -1,5 +1,7 @@
 const io = require('socket.io-client');
 const socket = io('http://localhost:9999');
+const spawn = require('child_process');
+
 socket.on('connect', handleConnect);
 socket.on('hashblock', handleMessage);
 socket.on('disconnect', handleDisconnect);
@@ -12,8 +14,20 @@ function handleConnect() {
 
 function handleMessage(websocketMessage) {
   console.log('websocket message:', websocketMessage);
+  const child = spawn('rm -rf tmp/index.pid && node scripts/sync.js index update && node scripts/sync.js market update', );
+  child.stdout.setEncoding('utf8');
+  // use child.stdout.setEncoding('utf8'); if you want text chunks
+  child.stdout.on('data', (chunk) => {
+    console.log('child process:', chunk);
+  });
+
+  child.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
 }
 
 function handleDisconnect() {
   console.log('websocket disconnect.');
 }
+
+
